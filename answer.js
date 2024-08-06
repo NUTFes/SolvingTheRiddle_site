bcc7c3bc6de783dd87cb728e192988568bfda75a
Hasegawa Nana <nana@Mac-2.local>
2024-08-06 15:48:56 +0900

    [change] answer.js

--- answer.js
+++ answer.js
@@ -1,17 +1,8 @@
 function checkKeyword() {
     var keyword = $('#keywordInput').val().toUpperCase(); // 入力を大文字に統一
     switch (keyword) {
-        case 'あいうえお':
+        case 'ラストスパート':
             $('#result').html('<img src="congrats.jpg" alt="Congratulations!"/>');
-            break;
-        case 'GIDAI':
-            $('#result').text('これは最初のヒントです。');
-            break;
-        case 'SOLUTION':
-            $('#result').text('次のヒント：解決策に近づいています！');
-            break;
-        case 'FINAL':
-            $('#result').text('最後のヒント：これがキーポイントです！');
             break;
         default:
             $('#result').text('無効なキーワードです。再試行してください。');
