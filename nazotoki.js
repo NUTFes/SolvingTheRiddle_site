4159b7310812a0654cf72102383bdbbbf5637e12
Hasegawa Nana <nana@Mac-2.local>
2024-08-06 14:08:23 +0900

    [change] moved files

--- nazotoki.js
+++ nazotoki.js
@@ -1,7 +1,7 @@
 function checkKeyword() {
     var keyword = $('#keywordInput').val().toUpperCase(); // 入力を大文字に統一
-    switch(keyword) {
-        case 'NUTFES':
+    switch (keyword) {
+        case 'あいうえお':
             $('#result').html('<img src="congrats.jpg" alt="Congratulations!"/>');
             break;
         case 'GIDAI':
