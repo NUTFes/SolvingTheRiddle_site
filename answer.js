21e67f244389fa4cfe5f83ad600ba25c846c129f
Hasegawa Nana <nana@Mac-2.local>
2024-08-11 21:11:11 +0900

    [change] index.html,answer.js, hint.js

--- answer.js
+++ answer.js
@@ -1,11 +1,13 @@
-function checkKeyword() {
-    var keyword = $('#keywordInput').val().toUpperCase(); // 入力を大文字に統一
+function checkAnswerKeyword() {
+    var keyword = $("#keywordInput").val().toUpperCase(); // 入力を大文字に統一
     switch (keyword) {
-        case 'ラストスパート':
-            $('#result').html('<img src="congrats.jpg" alt="Congratulations!"/>');
+        case "ラストスパート":
+            $("#answerResult").html(
+                '<img src="congrats.jpg" alt="Congratulations!"/>'
+            );
             break;
         default:
-            $('#result').text('無効なキーワードです。再試行してください。');
+            $("#answerResult").text("無効なキーワードです。再試行してください。");
             break;
     }
 }
--- hint.js
+++ hint.js
@@ -1,32 +1,42 @@
-function checkKeyword() {
-    var keyword = $('#Keywordinput').val().toUpperCase(); // 入力を大文字に統一
+function checkHintKeyword() {
+    var keyword = $("#hintInput").val().toUpperCase(); // 入力を大文字に統一
     switch (keyword) {
-        case 'スライド2':
-            $('#result').html('まずはそれぞれの漢字の読み方を書いてみましょう。');
+        case "スライド2":
+            $("#hintResult").html("まずはそれぞれの漢字の読み方を書いてみましょう。");
             break;
-        case 'スライド4':
-            $('#result').text('素？・？木、染？・？亭から考えると分かりやすいかもしれません。');
+        case "スライド4":
+            $("#hintResult").text(
+                "素？・？木、染？・？亭から考えると分かりやすいかもしれません。"
+            );
             break;
-        case 'スライド6':
-            $('#result').text('縦読みの単語が4つ並んでいる様です。');
+        case "スライド6":
+            $("#hintResult").text("縦読みの単語が4つ並んでいる様です。");
             break;
-        case 'スライド8':
-            $('#result').text('占という字の1・2画目を抜き出すとカタカナのトになります。');
+        case "スライド8":
+            $("#hintResult").text(
+                "占という字の1・2画目を抜き出すとカタカナのトになります。"
+            );
             break;
-        case 'スライド10':
-            $('#result').text('Aが正直者だとするとDも正直者になりますが、正直者は1人という部分と矛盾します。同様に考えてみましょう。');
+        case "スライド10":
+            $("#hintResult").text(
+                "Aが正直者だとするとDも正直者になりますが、正直者は1人という部分と矛盾します。同様に考えてみましょう。"
+            );
             break;
-        case 'スライド12':
-            $('#result').text('エレベーター、コンタクトレンズ、マカロニ、サックスのようです。');
+        case "スライド12":
+            $("#hintResult").text(
+                "エレベーター、コンタクトレンズ、マカロニ、サックスのようです。"
+            );
             break;
-        case 'スライド14':
-            $('#result').text('11という数字がアに対応してそうです。下に五十音表にあてはめると...');
+        case "スライド14":
+            $("#hintResult").text(
+                "11という数字がアに対応してそうです。下に五十音表にあてはめると..."
+            );
             break;
-        case 'スライド16':
-            $('#result').text('ラストスパートが答え＝まだラストではない。');
+        case "スライド16":
+            $("#hintResult").text("ラストスパートが答え＝まだラストではない。");
             break;
         default:
-            $('#result').text('無効なキーワードです。再試行してください。');
+            $("#hintResult").text("無効なキーワードです。再試行してください。");
             break;
     }
 }
