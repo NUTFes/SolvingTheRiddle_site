8084690b16d1bb685f34954e64df4f4f3010a137
nana1130 <23.n.hasegawa.nutfes@gmail.com>
2024-08-17 14:38:27 +0900

    [change] hint.js

--- hint.js
+++ hint.js
@@ -1,40 +1,40 @@
 function checkHintKeyword() {
     var keyword = $("#hintInput").val().toUpperCase(); // 入力を大文字に統一
     switch (keyword) {
-        case "スライド2":
+        case "2":
+            $("#hintResult").text(
+                "エレベーター、コンタクトレンズ、マカロニ、サックスのようです。");
+            break;
+        case "3":
+            $("#hintResult").text(
+                "素？・？木、染？・？亭から考えると分かりやすいかもしれません。");
+            break;
+        case "4":
+            $("#hintResult").text(
+                "11という数字がアに対応してそうです。下に五十音表にあてはめると...");
+            break;
+        case "5":
+            $("#hintResult").text(
+                "占という字の1・2画目を抜き出すとカタカナのトになります。");
+            break;
+        case "6":
+            $("#hintResult").text(
+                "Aが正直者だとするとDも正直者になりますが、正直者は1人という部分と矛盾します。同様に考えてみましょう。");
+            break;
+        case "7":
             $("#hintResult").html("まずはそれぞれの漢字の読み方を書いてみましょう。");
             break;
-        case "スライド4":
-            $("#hintResult").text(
-                "素？・？木、染？・？亭から考えると分かりやすいかもしれません。"
-            );
-            break;
-        case "スライド6":
+        case "8":
             $("#hintResult").text("縦読みの単語が4つ並んでいる様です。");
             break;
-        case "スライド8":
-            $("#hintResult").text(
-                "占という字の1・2画目を抜き出すとカタカナのトになります。"
-            );
-            break;
-        case "スライド10":
-            $("#hintResult").text(
-                "Aが正直者だとするとDも正直者になりますが、正直者は1人という部分と矛盾します。同様に考えてみましょう。"
-            );
-            break;
-        case "スライド12":
-            $("#hintResult").text(
-                "エレベーター、コンタクトレンズ、マカロニ、サックスのようです。"
-            );
-            break;
-        case "スライド14":
-            $("#hintResult").text(
-                "11という数字がアに対応してそうです。下に五十音表にあてはめると..."
-            );
-            break;
-        case "スライド16":
+
+        case "ラストスパート":
             $("#hintResult").text("ラストスパートが答え＝まだラストではない。");
             break;
+        case "コウコク":
+            $("#hintResult").text("パンフレットの広告になにかがある...？");
+            break;
+
         default:
             $("#hintResult").text("無効なキーワードです。再試行してください。");
             break;
